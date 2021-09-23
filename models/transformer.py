4df6b6e2d26faa6a2fe814df674af8dc54023f0d
Pranoy R <pranoyr@Pranoy-2.local>
2021-09-23 21:58:25 +0530

    updated detr

--- models/transformer.py
+++ models/transformer.py
@@ -48,17 +48,17 @@ class Transformer(nn.Module):
         # flatten NxCxHxW to HWxNxC  torch.Size([2, 256, 7, 7]) --> torch.Size([49, 2, 256])
         bs, c, h, w = src.shape
         src = src.flatten(2).permute(2, 0, 1)
-        print("src.shape:", src.shape)
+        # print("src.shape:", src.shape)
         pos_embed = pos_embed.flatten(2).permute(2, 0, 1)
         query_embed = query_embed.unsqueeze(1).repeat(1, bs, 1)
         mask = mask.flatten(1)
 
         tgt = torch.zeros_like(query_embed)
         memory = self.encoder(src, src_key_padding_mask=mask, pos=pos_embed)
-        print("memory.shape:", memory.shape)
+        # print("memory.shape:", memory.shape)
         hs = self.decoder(tgt, memory, memory_key_padding_mask=mask,
                           pos=pos_embed, query_pos=query_embed)
-        print("hs.shape:",hs.shape)
+        # print("hs.shape:",hs.shape)
         return hs.transpose(1, 2), memory.permute(1, 2, 0).view(bs, c, h, w)
 
 
