f6d20fee84871124a5de3b45f765550feb03652f
Pranoy R <pranoyr@Pranoy-2.local>
2021-09-23 23:16:58 +0530

    updated detr

--- main_vrd.py
+++ main_vrd.py
@@ -202,19 +202,20 @@ def main(args):
             model, criterion, data_loader_train, optimizer, device, epoch,
             args.clip_max_norm)
         lr_scheduler.step()
-        if args.output_dir:
-            checkpoint_paths = [output_dir / 'checkpoint.pth']
-            # extra checkpoint before LR drop and every 100 epochs
-            if (epoch + 1) % args.lr_drop == 0 or (epoch + 1) % 100 == 0:
-                checkpoint_paths.append(output_dir / f'checkpoint{epoch:04}.pth')
-            for checkpoint_path in checkpoint_paths:
-                utils.save_on_master({
-                    'model': model_without_ddp.state_dict(),
-                    'optimizer': optimizer.state_dict(),
-                    'lr_scheduler': lr_scheduler.state_dict(),
-                    'epoch': epoch,
-                    'args': args,
-                }, checkpoint_path)
+        # if args.output_dir:
+        torch.save(model_without_ddp.state_dict(), "detd_vrd_model.pth")
+            # checkpoint_paths = [output_dir / 'checkpoint.pth']
+            # # extra checkpoint before LR drop and every 100 epochs
+            # if (epoch + 1) % args.lr_drop == 0 or (epoch + 1) % 100 == 0:
+            #     checkpoint_paths.append(output_dir / f'checkpoint{epoch:04}.pth')
+            # for checkpoint_path in checkpoint_paths:
+            #     utils.save_on_master({
+            #         'model': model_without_ddp.state_dict(),
+            #         'optimizer': optimizer.state_dict(),
+            #         'lr_scheduler': lr_scheduler.state_dict(),
+            #         'epoch': epoch,
+            #         'args': args,
+            #     }, checkpoint_path)
 
         # test_stats, coco_evaluator = evaluate(
         #     model, criterion, postprocessors, data_loader_val, base_ds, device, args.output_dir
