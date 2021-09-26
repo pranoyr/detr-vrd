f10c87f4884461d1a8d606cc54f31066bde6753a
Pranoy R <pranoyr@Pranoy-2.local>
2021-09-26 15:25:57 +0530

    updated detr

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,6 +3,7 @@ import torch.utils.data
 import torchvision
 
 from .coco import build as build_coco
+from .vrd import build as build_vrd
 
 
 def get_coco_api_from_dataset(dataset):
@@ -18,6 +19,8 @@ def get_coco_api_from_dataset(dataset):
 def build_dataset(image_set, args):
     if args.dataset_file == 'coco':
         return build_coco(image_set, args)
+    if args.dataset_file == 'vrd':
+        return build_vrd(image_set, args)
     if args.dataset_file == 'coco_panoptic':
         # to avoid making panopticapi required for coco
         from .coco_panoptic import build as build_coco_panoptic
