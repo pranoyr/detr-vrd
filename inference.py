97341f295c6edddbe6e28ab4a9d6d2dc28e45f0e
Pranoy R <pranoyr@Pranoy-2.local>
2021-10-01 20:59:01 +0530

    sfdsf

--- inference.py
+++ inference.py
@@ -152,7 +152,8 @@ model = model.to(device)
 
 
 # model.to("cpu")
-state_dict =torch.load("/Volumes/Neuroplex/detr-r50-e632da11.pth", map_location=torch.device('cpu'))["model"]
+# state_dict =torch.load("/Volumes/Neuroplex/model.pth", map_location=torch.device('cpu'))["model"]
+state_dict = torch.load("/Volumes/Neuroplex/model.pth", map_location=torch.device('cpu'))
 model.load_state_dict(state_dict, strict=False)
 model.eval()
 
@@ -252,22 +253,23 @@ In the following cells, we define the mapping from class indices to names.
 """
 
 # COCO classes
-CLASSES = [
-    'N/A', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A',
-    'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
-    'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack',
-    'umbrella', 'N/A', 'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
-    'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
-    'skateboard', 'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass',
-    'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
-    'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
-    'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A',
-    'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
-    'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A',
-    'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
-    'toothbrush'
-]
+# CLASSES = [
+#     'N/A', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+#     'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A',
+#     'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
+#     'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack',
+#     'umbrella', 'N/A', 'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+#     'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+#     'skateboard', 'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass',
+#     'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+#     'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
+#     'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A',
+#     'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
+#     'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A',
+#     'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
+#     'toothbrush'
+# ]
+CLASSES = ("aeroplane","bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor")
 
 import os 
 
