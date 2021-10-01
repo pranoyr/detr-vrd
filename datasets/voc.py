ead71acf1bc03e6f2cbe946c29012d662f194ab9
Pranoy R <pranoyr@Pranoy-2.local>
2021-10-01 18:59:00 +0530

    sfdsf

--- datasets/voc.py
+++ datasets/voc.py
@@ -75,9 +75,7 @@ class VOCDataset:
 	
 		logging.info("No labels file, using default VOC classes.")
 		
-		self.class_names = ('bicycle', 'truck', 'tt', 'bus', 'car', 'motorbike', 'autorickshaw')
-
-		
+		self.class_names = ("bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor")
 		self.class_dict = {class_name: i for i,
 						   class_name in enumerate(self.class_names)}
 		
