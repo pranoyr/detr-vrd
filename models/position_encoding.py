3030adf54ddadbbde8ebf762fb210bdbf1330aa8
Pranoy R <pranoy@firstsense.tech>
2022-04-07 14:52:10 +0530

    tensor query updated

--- models/position_encoding.py
+++ models/position_encoding.py
@@ -37,6 +37,9 @@ class PositionEmbeddingSine(nn.Module):
             y_embed = y_embed / (y_embed[:, -1:, :] + eps) * self.scale
             x_embed = x_embed / (x_embed[:, :, -1:] + eps) * self.scale
 
+        # dim_t = torch.arange(self.num_pos_feats, dtype=torch.float32, device=x.device)
+        # dim_t = self.temperature ** (2 * (dim_t // 2) / self.num_pos_feats)
+
         dim_t = torch.arange(self.num_pos_feats, dtype=torch.float32, device=x.device)
         t_ = torch.div(dim_t, 2, rounding_mode='floor')
         dim_t = self.temperature ** (2 * (t_) / self.num_pos_feats)
