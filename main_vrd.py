a1ce2d121e9ec5f4ca3efa6c1f6e9a691521f095
Pranoy R <pranoy@firstsense.tech>
2022-04-11 12:06:09 +0530

    updated with file name

--- main_vrd.py
+++ main_vrd.py
@@ -181,7 +181,7 @@ def main(args):
                 args.resume, map_location='cpu', check_hash=True)
         else:
             checkpoint = torch.load(args.resume)
-        model_without_ddp.load_state_dict(checkpoint, strict=True)
+        model_without_ddp.load_state_dict(checkpoint["model"], strict=True)
         if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
             optimizer.load_state_dict(checkpoint['optimizer'])
             lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
