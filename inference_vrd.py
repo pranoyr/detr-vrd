00a4c9c9c5e7a289f43c3ea29b5eaec78f4273a7
Pranoy R <pranoy@firstsense.tech>
2022-04-15 17:35:35 +0530

    saving model...

--- inference_vrd.py
+++ inference_vrd.py
@@ -146,7 +146,7 @@ To try DETRdemo model on your own image just change the URL below.
 """
 
 # url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
-im = Image.open("/home/pranoy/Desktop/vrd_sample/183144994_1e009b7fc6_b.jpg")
+im = Image.open("/home/pranoy/Desktop/vrd_sample/38457647_011d630cc1_o.jpg")
 img = np.array(im)
 draw = img.copy()
 draw_rlp = cv2.cvtColor(draw, cv2.COLOR_RGB2BGR)
